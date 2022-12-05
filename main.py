c0fc5a739c62ba73b36d85a9d7cea9a3a61dead7
Ivan Okello <74784829+Ivandamien@users.noreply.github.com>
2022-12-05 19:32:35 +0300

    Updated main.py
    Another implementation of animated robot with python pygame
    
    Co-Authored-By: kakwiri <85194481+kakwiri@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,71 +1,71 @@
 
-# # @tigercoder5067
-# # 1 year ago
-# #This is source code for everyone
-# import pygame
-# import glob
+# @tigercoder5067
+# 1 year ago
+#This is source code for everyone
+import pygame
+import glob
 
-# size = width,height = 500,600
+size = width,height = 500,600
 
-# fps = 20
-# white = (255,255,255)
+fps = 20
+white = (255,255,255)
 
-# class MySprite(pygame.sprite.Sprite):
-#     def __init__(self, action):
-#         super(MySprite, self).__init__()
-#         im = glob.glob(f'{action}*.png')
-#         lenim = len(im[0])
-#         self.images =[pygame.image.load(img) for img in glob.glob(f'{action}*.png')
-#                       if len(img) == lenim]
-#         self.images2 = [pygame.image.load(img) for img in glob.glob(f'{action}*.png')
-#                       if len(img) > lenim] 
-#         self.images.extend(self.images2)
-#         self.index = 0
-#         self.rect = pygame.Rect(5,5,150,198)
-#     def update(self):
-#         if self.index >= len(self.images):
-#             self.index =0
-#         self.image = self.images[self.index]
-#         self.index += 1
-
-
-# def action(action):
-#     my_sprite = MySprite(action)
-#     my_group = pygame.sprite.Group(my_sprite)
-#     return my_group
-
-# def main():
-#     pygame.init()
-#     screen =pygame.display.set_mode(size)
-#     pygame.display.set_caption('how to animate sprite')
-#     my_sprite = MySprite("idle ")
-#     my_group = pygame.sprite.Group(my_sprite)
-#     clock = pygame.time.Clock()
-#     loop = 1
-#     while loop:
-#         for event in pygame.event.get():
-#             if event.type == pygame.QUIT:
-#                 loop = 0
-#             if event.type == pygame.KEYDOWN:
-#                 if event.key == pygame.K_x:
-#                     my_group = action("dead ")
-#                 if event.key == pygame.K_i or event.key == pygame.K_LEFT:
-#                     my_group = action("Idle ")
-#                 if event.key == pygame.K_UP or event.key == pygame.K_SPACE:
-#                     my_group = action("jump ")
-
-#                 if event.key == pygame.K_r or event.key == pygame.K_RIGHT :
-#                     my_group = action("run ")
-#                 if event.key == pygame.K_q:
-#                     my_group = action("walk ")
+class MySprite(pygame.sprite.Sprite):
+    def __init__(self, action):
+        super(MySprite, self).__init__()
+        im = glob.glob(f'{action}*.png')
+        lenim = len(im[0])
+        self.images =[pygame.image.load(img) for img in glob.glob(f'{action}*.png')
+                      if len(img) == lenim]
+        self.images2 = [pygame.image.load(img) for img in glob.glob(f'{action}*.png')
+                      if len(img) > lenim] 
+        self.images.extend(self.images2)
+        self.index = 0
+        self.rect = pygame.Rect(5,5,150,198)
+    def update(self):
+        if self.index >= len(self.images):
+            self.index =0
+        self.image = self.images[self.index]
+        self.index += 1
 
 
-#         my_group.update()
-#         screen.fill(white)
-#         my_group.draw(screen)
-#         pygame.display.update()
-#         clock.tick(fps)
-#     pygame.quit()
+def action(action):
+    my_sprite = MySprite(action)
+    my_group = pygame.sprite.Group(my_sprite)
+    return my_group
 
-# if __name__ == '__main__':
-#      main()
+def main():
+    pygame.init()
+    screen =pygame.display.set_mode(size)
+    pygame.display.set_caption('how to animate sprite')
+    my_sprite = MySprite("idle ")
+    my_group = pygame.sprite.Group(my_sprite)
+    clock = pygame.time.Clock()
+    loop = 1
+    while loop:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                loop = 0
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_x:
+                    my_group = action("dead ")
+                if event.key == pygame.K_i or event.key == pygame.K_LEFT:
+                    my_group = action("Idle ")
+                if event.key == pygame.K_UP or event.key == pygame.K_SPACE:
+                    my_group = action("jump ")
+
+                if event.key == pygame.K_r or event.key == pygame.K_RIGHT :
+                    my_group = action("run ")
+                if event.key == pygame.K_q:
+                    my_group = action("walk ")
+
+
+        my_group.update()
+        screen.fill(white)
+        my_group.draw(screen)
+        pygame.display.update()
+        clock.tick(fps)
+    pygame.quit()
+
+if __name__ == '__main__':
+     main()
